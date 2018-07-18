1de0b586f4f4c50f52c4ef5c887c4ce6d1ca0411
Eddie <tamamu.1r1s@gmail.com>
2018-07-18 19:54:03 +0900

    Add essential files

---
+++ src/parser.js
@@ -0,0 +1,7 @@
+function parse() {
+  console.log("Parsed");
+}
+
+module.exports = {
+  parse: parse
+};
---
+++ test/index.test.js
@@ -0,0 +1,6 @@
+
+const self = require('../src/index')
+test('All test', () => {
+  self.parse()
+  expect(true).toBe(true)
+})
