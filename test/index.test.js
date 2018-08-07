3cdd7659ea62ddb9b89874ad187e26dbc58d6736
Eddie <tamamu.1r1s@gmail.com>
2018-08-08 01:35:58 +0900

    Add parse test

--- test/index.test.js
+++ test/index.test.js
@@ -1,6 +1,42 @@
 
 const self = require('../src/index')
-test('All test', () => {
-  self.parse()
-  expect(true).toBe(true)
+test('Parse a command tag', () => {
+  expect(self.parse('[cm]'))
+    .toBe([{type: 'command' , name: 'cm', args: {}}])
+})
+
+test('Parse a command tag with arguments', () => {
+  expect(self.parse('[wait time=200]'))
+    .toBe([{type: 'command', name: 'wait', args: {'time': 200}}])
+})
+
+test('Parse a command tag with arguments', () => {
+  expect(self.parse('[image storage="bg0" page=fore layer=base]'))
+    .toBe([{type: 'command', name: 'image',
+      args: {'storage': 'bg0', 'page': 'fore', 'layer': 'base'}}])
+})
+
+test('Parse a label', () => {
+  expect(self.parse('*start'))
+    .toBe([{type: 'label', name: 'start'}])
+})
+
+test('Parse a comment', () => {
+  expect(self.parse('; line comment'))
+    .toBe([])
+})
+
+test('Parse text', () => {
+  expect(self.parse('Hello, world!'))
+    .toBe([{type: 'text', content: "Hello, world!"}])
+})
+
+test('Parse a command line', () => {
+  expect(self.parse('@wait time=200'))
+    .toBe([{type: 'command', name: 'wait', args: {'time': 200}}])
+})
+
+test('Parse a command line with indent', () => {
+  expect(self.parse('   @cm'))
+    .toBe([{type: 'command', name: 'cm', args: {}}])
 })
